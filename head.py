1eb0c4d5e2b464b37d5df90c086c6163c53be6ec
Alex Rieser <rieser.alex@me.com>
2025-01-30 20:46:03 +0100

    seperate files

--- head.py
+++ head.py
@@ -1,6 +1,93 @@
 import movementFunctions as mv
 import trackingFunctions as tr
 
+xAngleAdjustor = 0
+yAngleAdjustor = 0
+
+pastXError = []
+pastYError = []
+pastVectorSpeed = []
+
+def evaluateAdjustion(xError, yError, vectorSpeed):
+    pastXError.append(xError)
+    pastYError.append(yError)
+    pastVectorSpeed.append(vectorSpeed)
+
+    averagXError = 0
+    averagYError = 0
+    averageVectorSpeed = 0
+
+    if len(pastXError) > 6:
+        for y in pastYError:
+            averageXError += y
+
+        for x in pastXError:
+            averageYError += x
+
+        for z in pastVectorSpeed:
+            averageVectorSpeed += z
+        
+        averageYError = averageYError / 5
+        averageXError = averageXError / 5
+        averageVectorSpeed = averageVectorSpeed / 5
+
+        pastXError.pop(0)
+        pastYError.pop(0)
+        pastVectorSpeed.pop(0)
+
+    if averageVectorSpeed < 5:
+        if abs(averageXError) > 90:
+            xAngleAdjustor += (averageXError / 90) * -0.5
+            
+            pastXError = []
+            pastYError = []
+            pastVectorSpeed = []
+
+        if abs(averageYError) > 90:
+            yAngleAdjustor += (averageYError / 90) * -0.5
+
+            pastXError = []
+            pastYError = []
+            pastVectorSpeed = []
+        
+
+def evaluateAngle(coords, xVelocity, yVelocity, vectorSpeed):
+    evaluateAdjustion(coords[0] - goalX, coords[1] - goalY, vectorSpeed)
+
+    xAngle = 0
+    yAngle = 0
+
+    xAngle = xVelocity / 40 * -4
+
+    yAngle = yVelocity / 40 * -4
+
+    value = (coords[0] - xGoal) / 450
+    xAngle += (value ** 0.8 if value >= 0 else -(-value) ** 0.8) * -3
+    
+    value = (coords[1] - yGoal) / 450
+    yAngle += (value ** 0.8 if value >= 0 else -(-value) ** 0.8) * -3
+
+    return xAngle + xAngleAdjustor, yAngle + yAngleAdjustor
+
+goalX = 600
+goalY = 600
+
+tick = 0
+
+mv.centerPlate()
+
 while True:
+    tick += 1
+
     coords = tr.find_orange_object_coordinates()
-    print(coords)
+    if coords:
+        velocityData = tr.getBallVelocityVector(coords)
+        Xvelocity = velocityData[0]
+        Yvelocity = velocityData[1]
+        vectorspeed = velocityData[2]
+
+        angles = tr.evaluateAngle(coords, Xvelocity, Yvelocity, vectorspeed)
+
+        mv.setPlateAngle(angles[0], angles[1])
+    else:
+        print("no object found")
